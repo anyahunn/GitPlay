7e55c68564a41feac16795308bbdcea559974c01
wasadmin <wasadmin@example.com>
2025-09-10 09:49:29 -0400

    added git steps

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 console.log('hi')
+
+// steps:
+// create folder GitPlay
+// git init 
+// git add -A
+// git commit -m "first commit"
+// git branch-M main 
+// git remote add origin yourgithubrepo
+// git push -u origin main
